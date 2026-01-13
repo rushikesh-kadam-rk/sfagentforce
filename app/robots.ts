c9f140d13ab862f5fa647dadad35b3d6d76a7bfb
Rushikesh Kadam <116633744+rushikesh-kadam-rk@users.noreply.github.com>
2026-01-13 09:13:31 +0530

    Update sitemap URL in robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/', '/admin/'],
     },
-    sitemap: 'https://sfai.netlify.app/sitemap.xml',
+    sitemap: 'https://rkagentforce.netlify.app/sitemap.xml',
   }
 }
 
